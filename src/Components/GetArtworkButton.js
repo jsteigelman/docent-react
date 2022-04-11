0c12aefcf35af7db341d664c51f4c77da08dc7e2
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-11 14:26:02 -0400

    get artwork button scrolls to top of page on mobile devices

--- src/Components/GetArtworkButton.js
+++ src/Components/GetArtworkButton.js
@@ -1,10 +1,15 @@
 const GetArtworkButton = ({ museum, changeApi }) => {
 
-    const getApi = museum === 'Met' ? () => changeApi('Met') : () => changeApi('AIC')
+    // const getApi = museum === 'Met' ? () => changeApi('Met') : () => changeApi('AIC')
+
+    const handleClick = () => {
+      museum === 'Met' ? changeApi('Met') : changeApi('AIC')
+      window.scrollTo({top: 0, behavior: 'smooth'})
+    }
 
     return (
         <div className='getArtworkButtonContainer'>
-        <button className='getArtworkButton' onClick={getApi}>
+        <button className='getArtworkButton' onClick={handleClick}>
           Show me another work
         </button>
         <p id='sendEmailLink'>
